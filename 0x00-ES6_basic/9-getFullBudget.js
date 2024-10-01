c99be5db7e0c00956112ea1f87c2305ee935a3c0
Meargteame <mearegteame99995555@gmail.com>
2024-10-01 04:35:12 +0300

    es6 basics understanding the concepts and making them solid using the tasks

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,16 @@
+// 9-getFullBudget.js
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+
+  return {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+}
