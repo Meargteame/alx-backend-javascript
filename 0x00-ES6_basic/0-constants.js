28e955e298f78057e0f9859c6cb61534e2c3e3a9
Meargteame <mearegteame99995555@gmail.com>
2024-10-01 04:39:41 +0300

    update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,3 @@
-// 0-constants.js
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
@@ -11,6 +10,6 @@ export function taskFirst() {
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
+  
     return combination;
   }
-  
