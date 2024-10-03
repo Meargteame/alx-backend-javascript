418b6e5810173062574a0ee0607fd4d5314a724b
Meargteame <mearegteame99995555@gmail.com>
2024-10-03 15:15:19 +0300

    classroom class updated

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,6 @@
-
-export default class ClassRoom{
-
-	constructor(maxStudentsSize){
-
-		this._maxStudentsSize = maxStudentsSize;
+export default class ClassRoom {
+	constructor(maxStudentsSize) {
+	  this._maxStudentsSize = maxStudentsSize;
 	}
-}
-
-
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+  }
+  
